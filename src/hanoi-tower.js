7234512df2a1a898208b95833e6896e5b96a4a5a
Nurlan Zhumakhan <nurlanzumahan@gmail.com>
2020-10-13 11:10:00 +0600

    feat: add solution to carbon-dating, hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  numTransfers = 2**disksNumber - 1;   
+  timeTransfers = Math.floor(numTransfers * 3600 / turnsSpeed);
+  console.log(numTransfers)
+  console.log(timeTransfers)
+  return { turns: numTransfers, seconds: timeTransfers}
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,21 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
   let mass = [];
+  if (!Array.isArray(arr)) throw new Error("ERR!")
   for (let i = 0; i < arr.length; i++) {
-    if(!Array.isArray(arr)) return arr
-    if(arr[i] === '--discard-next'){
-    if(i < arr.length) i++
+    if (arr[i] === '--discard-next') {
+      if (i < arr.length) return i++
       i++
-    }else if(arr[i] === '--discard-prev'){
-    if(i === 0) i++
+    } else if (arr[i] === '--discard-prev') {
+      if (i === 0) return i++
       mass.splice(i - 1, 1)
-    }else if(arr[i] === '--double-next'){
-    if(i < arr.length) i++
-      mass.push(i + 1)
-    }else if(arr[i] === '--double-prev'){
-    if(i === 0) i++
-      mass.push(i)
-    }else{
+    } else if (arr[i] === '--double-next') {
+      if (i === arr.length) return i++
+      mass.push(arr[i + 1])
+    } else if (arr[i] === '--double-prev') {
+      if (i === 0) return i++
+      mass.push(arr[i - 1])
+    } else {
       mass.push(arr[i])
     }
   }
